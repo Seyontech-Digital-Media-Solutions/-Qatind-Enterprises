396196f656a8091628d0b120efb89b5fa15e062c
Swetha <swethasenthilkmr2@gmail.com>
2026-08-17 18:36:01 +0530

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/-Qatind-Enterprises/',
   plugins: [react()],
   server: {
     port: 3000,
